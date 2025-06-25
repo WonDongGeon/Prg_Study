2d8b072babd0f7b9392838d8096d563650b1dccb
WonDongGeon <xpdlf5103@naver.com>
2025-06-25 13:02:22 +0900

    Update OracleBoardDAO.java

--- Spring/Spring_Day07_BOARD[게시판 만들기(MVC)]/java/repository/OracleBoardDAO.java
+++ Spring/Spring_Day07_BOARD[게시판 만들기(MVC)]/java/repository/OracleBoardDAO.java
@@ -79,65 +79,17 @@ public class OracleBoardDAO implements BoardDAO {
 
 	@Override
 	public Board FindById(int id) {
-		String sql = "select * from board where id = ?";
-		try {
-			PreparedStatement ps = ds.getConnection().prepareStatement(sql);
-			ps.setInt(1, id);
-			ResultSet rs = ps.executeQuery();
-			while(rs.next()) {
-				Board bd = new Board();
-				bd.setId(rs.getInt("id"));
-				bd.setTitle(rs.getString("title"));
-				bd.setAuthor(rs.getString("author"));
-				bd.setCreatedate(rs.getDate("createdate"));
-				bd.setContent(rs.getString("content"));
-				bd.setAttachment(rs.getString("attachment"));
-				bd.setViewcnt(rs.getInt("viewcnt"));
-				bd.setType(rs.getString("type"));
-				rs.close();
-				ps.close();
-				return bd;
-			}
-		}catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
 		return null;
 	}
 
 	@Override
 	public int update(Board board) {
-		String sql = "update board set title=?,author=?,content=?,attachment=?,type=? where id=?";
-		try {
-			PreparedStatement ps = ds.getConnection().prepareStatement(sql);
-			ps.setString(1, board.getTitle());
-			ps.setString(2,  board.getAuthor());
-			ps.setString(3, board.getContent());
-			ps.setString(4, board.getAttachment());
-			ps.setString(5, board.getType());
-			ps.setInt(6, board.getId());
-			int result = ps.executeUpdate();
-			ps.close();
-			return result;
-		}catch (Exception e) {
-			e.printStackTrace();
-			return 0;
-		}
+		return 0;
 	}
 
 	@Override
 	public int delete(int id) {
-		String sql = "delete from board where id=?";
-		try {
-			PreparedStatement ps = ds.getConnection().prepareStatement(sql);
-			ps.setInt(1, id);
-			int result = ps.executeUpdate();
-			ps.close();
-			return result;
-		}catch (Exception e) {
-			e.printStackTrace();
-			return 0;
-		}
+		return 0;
 	}
 
 	@Override
